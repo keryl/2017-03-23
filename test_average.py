2dc24a280385aa265c9d54ed9c8d02432334bcdf
keryl <kerylklonye@gmail.com>
2017-03-23 16:34:17 +0300

    add testcase to check for input type

--- test_average.py
+++ test_average.py
@@ -7,5 +7,10 @@ class AverageTestCase(unittest.TestCase):
     def test_calculates_average_of_numbers_in_list(self):
         result = average.average([10,2,3,4,5])
         self.assertEqual(4.8, result)
+
+    def test_does_not_accept_strings(self):
+        result = average.average(["ten", "two", 3, 4, 5])
+        self.assertEqual("numbers only", result)
+
 if __name__ == "__main__":
     unittest.main()
